bbc4016146e8960e97ba1d73623b725c5ec386e2
leon-do <leondo123@gmail.com>
2024-09-09 16:41:04 -0400

    evn

---
+++ src/utils/constants.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prefer-const */
+import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
+import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const FACTORY_ADDRESS = '<V3_CORE_FACTORY_ADDRESS>' 
+export const WETH_ADDRESS = '<WETH_ADDRESS>'
+export const USD_TOKEN_ADDRESS = '<USD_TOKEN_ADDRESS>'
+export const USDC_WETH_03_POOL = '<USDC_WETH_03_POOL>'
+
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
+export let BI_18 = BigInt.fromI32(18)
+
+export let factoryContract = FactoryContract.bind(Address.fromString(FACTORY_ADDRESS))
